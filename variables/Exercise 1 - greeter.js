ac069e8c06751fb4f82e6bf55af840936a34c08d
Charles Morris Jr <cmorrisjr777@gmail.com>
2022-08-16 11:09:27 -0400

    Chapter 2 - Variables Exercises

---
+++ variables/Exercise 1 - greeter.js
@@ -0,0 +1,10 @@
+/*
+  Write a program named greeter.js that greets 'Victor' three times. Your
+  program should use a variable and not hard code the string value 'Victor' in each greeting.
+*/
+
+let myName = 'Victor';
+
+console.log(`Good Morning, ${myName}.`)
+console.log(`Good Afternoon, ${myName}.`)
+console.log(`Good Evening, ${myName}.`)
---
+++ variables/Exercise 2 - age.js
@@ -0,0 +1,11 @@
+/*
+  Write a program named age.js that includes someone's age and then calculates
+  and reports the future age in 10, 20, 30 and 40 years. Below is the output for someone 20 years old.
+*/
+
+let myAge = 27;
+
+console.log(`In 10 years, you will be ${myAge + 10} years old.`)
+console.log(`In 10 years, you will be ${myAge + 20} years old.`)
+console.log(`In 10 years, you will be ${myAge + 30} years old.`)
+console.log(`In 10 years, you will be ${myAge + 40} years old.`)
